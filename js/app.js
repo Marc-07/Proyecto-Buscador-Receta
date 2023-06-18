d670911f2afc65ac4515c08c56b73cf8f2b9b075
Marc-07 <maanraca01@gmail.com>
2023-06-18 15:52:35 -0500

    Se muesran las categorias en el select

--- js/app.js
+++ js/app.js
@@ -1,13 +1,29 @@
 function iniciarApp(){
 
+    const selectCategorias = document.querySelector('#categorias');
+
     obtenerCategorias();
 
     function obtenerCategorias(){
         const url = 'https://www.themealdb.com/api/json/v1/1/categories.php';
         fetch(url)
             .then(respuesta => respuesta.json())
-            .then(resultado => console.log(resultado))
+            .then(resultado => mostrarCategorias(resultado.categories))
     }
+
+    function mostrarCategorias(categorias = []){
+        categorias.forEach(categoria => {
+            const {strCategory} = categoria;
+            const option = document.createElement('OPTION');
+            option.value = categoria;
+            option.textContent = categoria.strCategory;
+
+            selectCategorias.appendChild(option);
+            
+        })
+
+    }
+
 }
 
 document.addEventListener('DOMContentLoaded', iniciarApp);
