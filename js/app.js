a2270848b95d28f1b80540297acefffd77da34ef
Marc-07 <maanraca01@gmail.com>
2023-06-21 12:14:21 -0500

    Se obtienen  las recetas de la API

--- js/app.js
+++ js/app.js
@@ -1,29 +1,41 @@
 function iniciarApp(){
 
     const selectCategorias = document.querySelector('#categorias');
+    selectCategorias.addEventListener('change', seleccionarCategoria);
 
     obtenerCategorias();
 
-    function obtenerCategorias(){
+    function obtenerCategorias (){
         const url = 'https://www.themealdb.com/api/json/v1/1/categories.php';
         fetch(url)
             .then(respuesta => respuesta.json())
-            .then(resultado => mostrarCategorias(resultado.categories))
+            .then ( resultado => mostrarCategorias(resultado.categories));
+
     }
 
     function mostrarCategorias(categorias = []){
-        categorias.forEach(categoria => {
+        categorias.forEach( categoria =>{
+
             const {strCategory} = categoria;
             const option = document.createElement('OPTION');
-            option.value = categoria;
-            option.textContent = categoria.strCategory;
-
+            option.value = strCategory;
+            option.textContent = strCategory;
             selectCategorias.appendChild(option);
-            
+           
         })
+
 
     }
 
+    function seleccionarCategoria(e){
+        const categoria = e.target.value;
+        const url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${categoria}`;
+        fetch(url)
+            .then(respuesta => respuesta.json())
+            .then(resultado => console.log(resultado));
+
+
+    }
 }
 
 document.addEventListener('DOMContentLoaded', iniciarApp);
